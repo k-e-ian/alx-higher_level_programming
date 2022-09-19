d0773e53e0e5d05eedeb21b0e9c081a1189f7de3
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-09-19 23:53:43 +0300

    turtle vs hare race

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a singly linked listint_t contains a cycle
+ * @list: listint_t singly linked list
+ * Return: Conditional 0 if there's no circle, 1 if there's a circle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *infront, *behind;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	behind = list->next;
+	infront = list->next->next;
+
+	while (infront && behind && behind->next)
+	{
+		if (infront == behind)
+			return (1);
+		behind = behind->next;
+		infront = infront->next->next;
+	}
+	return (0);
+}
