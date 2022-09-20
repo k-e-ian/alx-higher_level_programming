07129ea8c986ca7366bfbb939b0747254bf88f0d
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-09-20 15:44:33 +0300

    FIZZBUZZ

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for fizbuz in range(1, 101):
+        if (fizbuz % 3 == 0):
+            print("Fizz", end=" ")
+        if (fizbuz % 3 == 0 and fizbuz % 5 == 0):
+            print("FizzBuzz", end=" ")
+        elif (fizbuz % 5 == 0):
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(fizbuz), end=" ")
