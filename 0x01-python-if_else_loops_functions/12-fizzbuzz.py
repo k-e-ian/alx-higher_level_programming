13d9c044d97f6429124b1383a7e654f9bcaa352d
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-09-20 15:57:48 +0300

    fizzybuzzy

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for fizbuz in range(1, 101):
-        if (fizbuz % 3 == 0):
-            print("Fizz", end=" ")
-        elif (fizbuz % 3 == 0 and fizbuz % 5 == 0):
+        if (fizbuz % 3 == 0 and fizbuz % 5 == 0):
             print("FizzBuzz", end=" ")
+        elif (fizbuz % 3 == 0):
+            print("Fizz", end=" ")
         elif (fizbuz % 5 == 0):
             print("Buzz", end=" ")
         else:
