392fc3ea6c2089631de4161af91fe329c24d53b1
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-09-26 13:26:00 +0300

    python args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv)
+    count -= 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(count))
+
+    for index in range(count):
+        print("{:d}: {:s}".format(index + 1, sys.argv[index + 1]))
