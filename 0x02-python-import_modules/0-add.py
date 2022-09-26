6655ebf412cae0ffe484a6814abf94a2e2aee17c
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-09-26 12:49:11 +0300

    import modules 101

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-	a, b = 1, 2
-	print("{a:d} + {b:d} = (a + b)
-	return (a + b)
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-from add_0 import add
-
-def add():
-    a = 0
-    b = 2
-    print("{} + {} = {}").format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    add()
+def add(a, b):
+    return(a + b)
