60bb764b532ea75966bffd7d384798af46015600
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-13 06:43:51 +0300

    document

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+'''define a class square'''
 
 
 class Square:
+    '''represent a class square'''
 
     def __init__(self, size):
+        '''initialize a new square
+
+        Args:
+            size (int) : size of new square
+        '''
         self.__size = size
