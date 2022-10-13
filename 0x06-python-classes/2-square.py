b14c2d375da55596c35d916ac68361956f6e3b00
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-13 06:48:23 +0300

    size_square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
+'''define a class square'''
 
 class Square:
+    '''represent a square'''
+
     def __init__(self, size=0):
+        '''initialize a square
+
+        Args:
+            size (int): size of new square
+        '''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
