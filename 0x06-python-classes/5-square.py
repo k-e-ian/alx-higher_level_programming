0485b4f4e00f4c1a8e046f1e813286a5474cd8f7
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-13 07:06:33 +0300

    #squared

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+'''define a class square'''
+
 
 class Square:
+    '''represent square'''
     def __init__(self, size):
+        '''initialize square
+        Args:
+            size (int): size of square
+        '''
         self.size = size
 
     @property
     def size(self):
+        '''get square size'''
         return (self.__size)
 
     @size.setter
@@ -17,9 +25,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        '''size of area squared'''
         return (self.__size * self.__size)
 
     def my_print(self):
+        '''print square with # char'''
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
