82acaab803e7a0021689832939bcb74b0862c805
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-16 14:49:07 +0300

    singlylinkedlist

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,8 +2,14 @@
 
 
 class Node:
-
+    """Represent a node in a singly linked list"""
     def __init__(self, data, next_node=None):
+        """initialize a new node
+        Args:
+            data (int): the data of the new node
+            next_node (Node): the next node of the new node
+        """
+
         self.data = data
         self.next_node = next_node
 
@@ -29,10 +35,17 @@ class Node:
 
 
 class SinglyLinkedList:
+    """Represent a Singly linked list"""
     def __init__(self):
+        """initialize a new singly linked list"""
         self.__head = None
 
     def sorted_insert(self, value):
+        """insert a node at new singly linked list
+        in order
+        Args:
+            value (Node): the new Node to insert
+        """
         new = Node(value)
         if self.__head is None:
             new.next_node = None
@@ -49,6 +62,7 @@ class SinglyLinkedList:
             tmp.next_node = new
 
     def __str__(self):
+        """define the print() rep of a singly linked list"""
         values = []
         tmp = self.__head
         while tmp is not None:
