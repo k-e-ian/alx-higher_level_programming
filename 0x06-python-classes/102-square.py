36aeff5f90a2f5031967f638ef4dcbfc4b77d4d5
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-10-16 15:12:25 +0300

    square comparisons

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""Define a class Square"""
 
 
 class Square:
+    """Represent a Square"""
     def __init__(self, size=0):
+        """Initialize a square
+        Args:
+            size (int): size of the square
+        """
         self.size = size
 
     @property
     def size(self):
+        """get size of square"""
         return (self.__size)
 
     @size.setter
@@ -18,22 +25,29 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return are of square"""
         return (self.__size * self.__size)
 
     def __eq__(self, other):
+        """define the == comparison of a square"""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """define the != comparison of a square"""
         return self.area() != other.area()
 
     def __lt__(self, other):
+        """define the < comparison of a square"""
         return self.area() < other.area()
 
     def __le__(self, other):
+        """define the <= comparison of a square"""
         return self.area() <= other.area()
 
     def __gt__(self, other):
+        """define the > comparison of a square"""
         return self.area() > other.area()
 
     def __ge__(self, other):
+        """define the >= comparison of a square"""
         return self.area() >= other.area()
