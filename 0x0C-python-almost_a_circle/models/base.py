03d32710bd2a7346f02cf4818370247ed84b8636
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-11-01 02:26:22 +0300

    class method retuning instance with attribute set

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,3 +53,17 @@ class Base:
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''returns an instance with all attributes already set
+        Args:
+            **dictionary (dict): key=value attributes to initialize
+        '''
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
