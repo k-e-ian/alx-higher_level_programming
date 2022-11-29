aa62a1b1084c6aec0cdd10028ed15777bec3ea35
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-11-29 19:00:57 +0300

    insert new row into first_table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- lists all rows of the table first_table from database hbtn_0c_0
+-- in mYSQL server, all fields should be printed
+-- database will be passed as mysql command argument
+SELECT * FROM `first_table`;
