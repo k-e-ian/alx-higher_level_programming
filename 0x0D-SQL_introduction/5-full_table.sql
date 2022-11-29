872f94d842a7b9e576cf30cad035caa278331428
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-11-29 18:52:13 +0300

    full descreption print

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- print full description of the table first_table from hbtn_0c_0
+-- in MySQL server , database name will be passed as an arg in 
+-- mysql command
+SHOW CREATE TABLE `first_table`;
