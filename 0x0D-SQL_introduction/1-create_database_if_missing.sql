d33267960ab6445b07837c8fdb13701907f73499
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-11-29 18:25:38 +0300

    backtick

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- creates the database hbtn_0c_0 in MySQL server
 -- if hbtn_0c_0 already exists, script should not fail
-CREATE DATABASE IF NOT EXISTS `htbn_0c_0`;
+CREATE DATABASE IF NOT EXISTS htbn_0c_0;
