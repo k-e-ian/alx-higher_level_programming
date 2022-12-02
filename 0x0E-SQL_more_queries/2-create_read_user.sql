24dbad67ca5c10427258c780bc001f7af7a7788a
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-12-02 06:55:05 +0300

    more sql querries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,6 @@
--- creates database hbtn_0d)2 and user_0d_2
--- user has SELECT privileges on hbtn_0d_2 with password
--- user_0d_2_pwd
-CREATE DATABASE
-	IF NOT EXISTS hbtn_0d_02;
-CREATE USER
-	IF NOT EXISTS 'user_0d_2'@'localhost'
-	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-	ON hbtn_0d_2.*
-	TO 'user_0d_2'@'localhost'
-	IDENTIFIED BY 'user_0d_2_pwd';
+-- creates the database hbtn_0d_2 and the user user_0d_2
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT ON hbtn_0d_2.* TO "user_0d_2"@"localhost";
 FLUSH PRIVILEGES;
