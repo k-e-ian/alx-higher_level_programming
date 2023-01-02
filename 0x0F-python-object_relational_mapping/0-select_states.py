8cacaba8d2f38b9ba71781c2fa566f53b3b3bc84
Ian Ewdin Kitembe <ianedwin@outlook.com>
2023-01-02 17:59:21 +0300

    listing all states from database hbtn

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+'''
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
+'''
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    '''not executed when imported'''
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8", user=user, passwd=password, db=db)
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
+    c.execute("SELECT * FROM `states` ORDER BY states.id")
     [print(state) for state in c.fetchall()]
