33e7b4c833650eef66a58434255d82f25112580d
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-01-04 18:19:43 +0300

    relationship city and state module documentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,16 @@
 '''
 import sys
 import MySQLdb
-
+'''
 if __name__ == "__main__":
-    '''not executed when imported'''
-
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
-                        , host="localhost", charset="utf8", port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+    db=sys.argv[3], host="localhost" charset="utf8", port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+'''
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
