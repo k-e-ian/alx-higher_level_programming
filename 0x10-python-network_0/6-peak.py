c9710a488f1da8c9f35f229b7642fc4bd0455e08
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-01-16 19:56:48 +0000

    catch me if you can

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 ''' function that finds a peak in a list of unsorted integers '''
 
+
 def find_peak(list_of_integers):
     '''function that finds peak from list_of_integers'''
     if type(list_of_integers) != list:
         return
     if len(list_of_integers) is 0:
-        return None
+        return (None)
     list_of_integers.sort(reverse=True)
     return list_of_integers[0]
