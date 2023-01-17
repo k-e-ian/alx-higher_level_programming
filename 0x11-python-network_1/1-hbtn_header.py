f3c2c4102802baf0d222f794ce0a96688b6f4ba1
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-01-17 12:59:35 +0000

    adding boilerplate code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,8 @@
 import sys
 import urllib.request
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    print(dict(response.headers).get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get('X-Request-Id'))
