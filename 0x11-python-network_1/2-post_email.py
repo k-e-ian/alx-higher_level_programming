58138b6bb49b1a1f5bbcd8d4e422c75c55a69a0e
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-01-17 13:04:09 +0000

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,10 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-val = {"email": sys.argv[2]}
-data = urllib.parse.urlencode(val).encode("ascii")
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(val).encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
