1ef1adf10d2333ef44835cd373539b2d2ea22beb
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-02-10 12:28:10 +0300

    semistandard2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
+// prints the title of a Star Wars Movie
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request.get(url + id, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
