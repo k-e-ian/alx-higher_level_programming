9f39e723be733ff1d4df9c360652d7e9f598ce5d
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-02-10 12:09:12 +0300

    print chara of Star Wars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
+const request = require('request');
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (error, response, body) => {
+    if (error) throw error;
+    else {
+      print_char(JSON.parse(body).characters, 0);
+    }
+  }
+);
+
+function print_char (person, idx) {
+  if (idx >= person.length) {
+    return;
+  }
+  request(person[idx], (error, response, body) => {
+    if (error) throw error;
+    else {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+      return print_char(person, ++idx);
     }
   });
 }
